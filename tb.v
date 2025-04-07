a75a76e7f868159900d9b8d2ce19dcea8a69b351
syeongkim <carsk0607@gmail.com>
2025-04-07 22:16:28 +0900

    gtkwave로 파형 확인

--- tb.v
+++ tb.v
@@ -1,7 +1,21 @@
 module testbench;
 
+reg clk;
+
 initial begin
     $display("Hello Verilog");
 end
 
+initial begin
+    $dumpfile("testbench.vcd");
+    $dumpvars(0, testbench);
+end
+
+initial begin
+    clk = 0;
+    #5 clk = 1;
+    #5 clk = 0;
+    #5 clk = 1;
+    #5 $finish;
+end
 endmodule
