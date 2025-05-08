fc35ae31fcc3fb694733d54c79078bc255348b86
syeongkim <carsk0607@gmail.com>
2025-05-08 15:41:05 +0900

    ADD project 2-ALU

--- Project_2/ALU.v
+++ Project_2/ALU.v
@@ -9,6 +9,33 @@ module ALU(
     output reg [15:0] C
     );
     
-    // FILLME
+    always @(*) begin
+        Cout = 0;
+
+        case(OP)
+            4'b0000: begin
+                {Cout, C} = A + B + Cin;
+            end
+            4'b0001: begin
+                C = A - (B + Cin);
+                Cout = ({1'b0, A} < ({1'b0, B} + Cin)) ? 1 : 0;
+            end
+            4'b0010: C = A;
+            4'b0011: C = ~(A & B);
+            4'b0100: C = ~(A | B);
+            4'b0101: C = ~(A ^ B);
+            4'b0110: C = ~A;
+            4'b0111: C = A & B;
+            4'b1000: C = A | B;
+            4'b1001: C = A ^ B;
+            4'b1010: C = A >> 1;
+            4'b1011: C = $signed(A) >>> 1;
+            4'b1100: C = {A[0], A[15:1]};
+            4'b1101: C = A << 1;
+            4'b1110: C = $signed(A) <<< 1;
+            4'b1111: C = {A[14:0], A[15]};
+            default: C = 16'h0000;
+        endcase
+    end
 
 endmodule
--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 Implement a synchronous 010 detector (Moore)
 - input: A sequence of binary numbers, 1 bit per clock
 - output: 1 if 010 is detected, 0 otherwise
+
+## Project 2
+1. Implement a 16-bit arithmetic & logic unit 
+- input: 16-bit A, 16-bit B, 1-bit Cin, 4-bit OP
+- output: 16-bit C, 1-bit Cout
+Implement a 16-bit 2-read/1-write register file
+- input: write(1-bit), clock(1-bit), reset(1-bit), three addresses(each 2-bit), one data(16-bit)
+- output: two read data(each 16 bits)
