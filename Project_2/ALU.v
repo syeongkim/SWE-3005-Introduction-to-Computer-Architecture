2db34bc2c09900a6e2242bef064b71ef2393abe4
syeongkim <carsk0607@gmail.com>
2025-05-08 15:06:38 +0900

    Project 2 initial commit

---
+++ Project_2/ALU.v
@@ -0,0 +1,14 @@
+`timescale 100ps / 100ps
+
+module ALU(
+    input [15:0] A,
+    input [15:0] B,
+    input Cin,
+    input [3:0] OP,
+    output reg Cout,
+    output reg [15:0] C
+    );
+    
+    // FILLME
+
+endmodule
---
+++ Project_2/RF.v
@@ -0,0 +1,17 @@
+`timescale 100ps / 100ps
+
+module RF(
+    input [1:0] addr1,
+    input [1:0] addr2,
+    input [1:0] addr3,
+    input [15:0] data3,
+    input write,
+    input clk,
+    input reset,
+    output reg [15:0] data1,
+    output reg [15:0] data2
+    );
+    
+    // FILLME
+    
+endmodule
